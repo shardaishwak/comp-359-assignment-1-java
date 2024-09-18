a4776da66bb86e7c9be5c64e108bf9b29a312a60
Ishwak Sharda <ishwak.sharda@gmail.com>
2024-09-18 14:11:13 -0700

    feat(sorting): (main) create abstract class

---
+++ src/core/AbstractSorting.java
@@ -0,0 +1,59 @@
+package core;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public abstract class AbstractSorting {
+    private Integer[] values;
+    private Integer[] states;
+    private boolean isSorted;
+
+    private int swapCount;
+    private int comparisonCount;
+
+    public AbstractSorting(Integer[] values) {
+        this.values = values;
+        this.states = new Integer[values.length];
+        Arrays.fill(states, -1);
+
+        this.isSorted  = false;
+        this.swapCount = 0;
+        this.comparisonCount = 0;
+
+    }
+
+    public void nextStep() {}
+
+
+    public void incrementComparator( ){
+        this.comparisonCount++;
+    }
+    public void incrementSwap() {
+        this.swapCount++;
+    }
+
+    public void activateState(int index) {
+        this.states[index] = 0;
+    }
+
+    public void deactivateState(int index) {
+        this.states[index] = 1;
+    }
+
+    // getters
+    public Integer[] getValues() {
+        return this.values;
+    }
+
+    public Integer[] getStates() {
+        return this.states;
+    }
+
+    public int getComparisonCount() {
+        return this.comparisonCount;
+    }
+
+    public int getSwapCount() {
+        return this.swapCount;
+    }
+}
